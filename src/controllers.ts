4a96fac79816bb7388e6991c6e3027d54fbe98e1
Taqinou <enzo.gazzoli@icloud.com>
2024-10-16 15:14:33 +0200

    mise en place HOME

--- src/controllers.ts
+++ src/controllers.ts
@@ -2,8 +2,8 @@ import { Request, Response } from 'express';
 
 export function getHomePage(req: Request, res: Response) {
 	res.render("home",{title: "PROJET TECH-WEB",
-    name: "Appart1",
-    description: "description"});
+    name: "HOME",
+    description: "Voici la page home"});
 }
 
 export function getAppartPage(req: Request, res: Response) {
