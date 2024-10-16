46741c0b2076d18d55966f78c726a72dee1d04df
Taqinou <enzo.gazzoli@icloud.com>
2024-10-16 15:05:29 +0200

    modif home

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 
 export function getHomePage(req: Request, res: Response) {
-	res.render("home",{title: "til",
-    name: "name",
+	res.render("home",{title: "PROJET TECH-WEB",
+    name: "Appart1",
     description: "description"});
 }
