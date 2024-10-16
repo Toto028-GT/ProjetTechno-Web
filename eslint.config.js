883e2bf69be0ffd841c631a77ab664b6fe66a25d
Toto028-GT <theocerq@gmail.com>
2024-10-16 15:28:12 +0200

    EslintSetup

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+const globals = require("globals");
+
+module.exports = [{
+    languageOptions: {
+        sourceType: "commonjs",
+        globals: {
+          ...globals.node
+        }
+    }
+}];
