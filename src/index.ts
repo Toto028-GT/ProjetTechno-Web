df346fe99592dab4351df72ed61ae02d80fcd57f
Toto028-GT <theocerq@gmail.com>
2024-10-16 13:59:05 +0200

    pingOK

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+import express from 'express' 
+
+const app = express()
+
+app.get("/ping", (req, res) => res.sendStatus(200));
+
+const server = app.listen(3000, () =>
+    console.log("Server started on http://localhost:3000"),
+  );
+
+
+
 console.log('Hello World')
