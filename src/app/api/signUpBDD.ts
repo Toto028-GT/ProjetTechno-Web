c074feb116d2f95057f30ce90eeb417ef4533698
ISinFor7 <yahia.berrouiguet.lilo@gmail.com>
2025-05-16 19:17:39 +0200

    signup clean

--- src/app/api/signUpBDD.ts
+++ src/app/api/signUpBDD.ts
@@ -1,23 +1,10 @@
-import { z } from 'zod';
 import { getUserByID,createNewUser } from "./models.ts";
-import Credentials from 'next-auth/providers/credentials';
 
 export async function newAccount(credentials: any) {
-    Credentials({
-            async authorize(credentials: Partial<Record<string, unknown>>, req: Request) {
-               const parsedCredentials = z
-                .object({ email: z.string().email(), password: z.string().min(5), nom: z.string(), prenom: z.string()})
-                .safeParse(credentials);
-
-                if (parsedCredentials.success) {
-                    const { email, password, nom, prenom } = parsedCredentials.data;
-                    const user = await getUserByID(email);
-                    if (user === null) {
-                        await createNewUser(email, password, nom, prenom);
-                        return { id: email, email, name: `${prenom} ${nom}` };
-                    }
-                }
-                return null;
-            }
-        }),
-};
+    const user = await getUserByID(credentials.email);
+    if (user === null) {
+        await createNewUser(credentials.email, credentials.password, credentials.nom, credentials.prenom);
+        return true;
+    }
+    return null;
+}
