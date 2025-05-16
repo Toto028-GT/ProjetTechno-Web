7ea9485c4e786dbbd950d68a131f34d3de71ed18
ISinFor7 <yahia.berrouiguet.lilo@gmail.com>
2025-05-16 19:25:03 +0200

    changement du if en conséquence et suppression typo

--- src/app/api/signUpBDD.ts
+++ src/app/api/signUpBDD.ts
@@ -2,8 +2,8 @@ import { getUserByID,createNewUser } from "./models.ts";
 
 export async function newAccount(credentials: any) {
     const user = await getUserByID(credentials.get("email"));
-    if (user?.email === null) {
-        await createNewUser(credentials.get("email"), credentials.get("getpassword"), credentials.get("nom"), credentials.get("prenom"));
+    if (user === null) {
+        await createNewUser(credentials.get("email"), credentials.get("password"), credentials.get("nom"), credentials.get("prenom"));
         return true;
     }
     return null;
