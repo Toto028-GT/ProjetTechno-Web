f14eb378e07af3d77f5de3b2560913f979fb1410
ISinFor7 <yahia.berrouiguet.lilo@gmail.com>
2025-05-16 19:09:22 +0200

    changement signup avec authorize de next

--- src/app/api/signUpBDD.ts
+++ src/app/api/signUpBDD.ts
@@ -1,17 +1,23 @@
 import { z } from 'zod';
 import { getUserByID,createNewUser } from "./models.ts";
+import Credentials from 'next-auth/providers/credentials';
 
 export async function newAccount(credentials: any) {
-            const parsedCredentials = z
+    Credentials({
+            async authorize(credentials: Partial<Record<string, unknown>>, req: Request) {
+               const parsedCredentials = z
                 .object({ email: z.string().email(), password: z.string().min(5), nom: z.string(), prenom: z.string()})
                 .safeParse(credentials);
 
-            if (parsedCredentials.success) {
-                const { email, password, nom, prenom } = parsedCredentials.data;
-                if (getUserByID(email)===null){ {
-                    createNewUser(email,password,nom,prenom);
+                if (parsedCredentials.success) {
+                    const { email, password, nom, prenom } = parsedCredentials.data;
+                    const user = await getUserByID(email);
+                    if (user === null) {
+                        await createNewUser(email, password, nom, prenom);
+                        return { id: email, email, name: `${prenom} ${nom}` };
+                    }
                 }
+                return null;
             }
-            return null;
-        }
+        }),
 };
