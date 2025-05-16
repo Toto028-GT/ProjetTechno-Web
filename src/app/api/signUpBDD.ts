86e2f72d4cabf0f05df214c0c8194d1167b6fb78
ISinFor7 <yahia.berrouiguet.lilo@gmail.com>
2025-05-16 19:23:02 +0200

    signup avec les bons types

--- src/app/api/signUpBDD.ts
+++ src/app/api/signUpBDD.ts
@@ -1,9 +1,9 @@
 import { getUserByID,createNewUser } from "./models.ts";
 
 export async function newAccount(credentials: any) {
-    const user = await getUserByID(credentials.email);
-    if (user === null) {
-        await createNewUser(credentials.email, credentials.password, credentials.nom, credentials.prenom);
+    const user = await getUserByID(credentials.get("email"));
+    if (user?.email === null) {
+        await createNewUser(credentials.get("email"), credentials.get("getpassword"), credentials.get("nom"), credentials.get("prenom"));
         return true;
     }
     return null;
