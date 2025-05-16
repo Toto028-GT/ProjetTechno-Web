b87070a86bab9422873e35c51504299c9d079ad1
ISinFor7 <yahia.berrouiguet.lilo@gmail.com>
2025-05-16 14:53:31 +0200

    init for git bash (windows)

---
+++ setup/init(git_bash).sh
@@ -0,0 +1,16 @@
+docker kill mon-container-mongodb
+docker rm mon-container-mongodb
+rm -rf ./docker_mongo 
+mkdir docker_mongo
+chmod -R 777 docker_mongo
+docker pull mongo
+docker run -d -p 27017:27017 --name mon-container-mongodb -u $(id -u) -v /$(pwd)/docker_mongo:/data/db -v /$(pwd):/app mongo
+
+# Wait for MongoDB to be ready
+until docker exec mon-container-mongodb mongosh --eval "db.runCommand({ connectionStatus: 1 })" &>/dev/null; do
+  echo "Waiting for MongoDB to be ready..."
+  sleep 1
+done
+
+#docker exec -it mon-container-mongodb mongosh --file /c/Users/Lilo/Documents/Code/Web/ProjetTechno-Web/setup/script.js
+docker exec -it mon-container-mongodb mongosh --eval //app/setup/script.js
