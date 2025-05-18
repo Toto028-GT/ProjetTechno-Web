3cb74c406d7e1bf867a0574f54e3dfcfb2ba356f
egazzoli <enzo.gazzoli@unif-jfc.fr>
2025-05-18 20:41:12 +0200

    minor fix (create appart page)

--- src/app/ui/insert-appart.tsx
+++ src/app/ui/insert-appart.tsx
@@ -19,14 +19,6 @@ export default function InsertApp() {
 
   return (
     <div className="bg-white/90 backdrop-blur-md shadow-xl rounded-2xl p-8 max-w-3xl mx-auto">
-      
-      <Bouton
-        destination="/"
-        style="flex items-center text-purple-600 hover:text-purple-700 font-semibold mb-6"
-      >
-        <ArrowLeft className="w-5 h-5 mr-2" />
-        Retour à l'accueil
-      </Bouton>
 
       <h1 className="text-3xl font-semibold text-gray-800 mb-6">Ajouter un logement</h1>
 
