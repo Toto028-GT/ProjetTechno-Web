34d6382052ceb6462ebe1c64ef4686f599869d5c
egazzoli <enzo.gazzoli@unif-jfc.fr>
2025-05-18 17:06:00 +0200

    minor fix init.sh

--- setup/init(git_bash).sh
+++ setup/init(git_bash).sh
@@ -1,16 +1,8 @@
-docker kill mon-container-mongodb
-docker rm mon-container-mongodb
-rm -rf ./docker_mongo 
 mkdir docker_mongo
 chmod -R 777 docker_mongo
 docker pull mongo
-docker run -d \
-  -p 27017:27017 \
-  --name mon-container-mongodb \
-  -v /c/Users/Enzo/Documents/ProjetTechno-Web/docker_mongo:/data/db \
-  -v /c/Users/Enzo/Documents/ProjetTechno-Web:/app \
-  mongo
-  
+docker run -d -p 27017:27017 --name mon-container-mongodb -u $(id -u) -v /$(pwd)/docker_mongo:/data/db -v /$(pwd):/app mongo
+
 # Wait for MongoDB to be ready
 until docker exec mon-container-mongodb mongosh --eval "db.runCommand({ connectionStatus: 1 })" &>/dev/null; do
   echo "Waiting for MongoDB to be ready..."
