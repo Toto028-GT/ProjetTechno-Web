37100f8b628762f06f2374e4000182f4cb8d83b5
ISinFor7 <yahia.berrouiguet.lilo@gmail.com>
2025-05-19 18:55:53 +0200

    new lint config

--- src/app/appartProfile/[id]/page.tsx
+++ src/app/appartProfile/[id]/page.tsx
@@ -3,7 +3,7 @@ import { ArrowLeft } from "lucide-react";
 import * as React from "react";
 import AppartInfo from "../temp.tsx";
 
-export default function AppartProfile({ params }: { params: { id: string } }) {
+export default async function AppartProfile({ params }: any) {
   const { id } = params;
   return (
     <div className="min-h-screen bg-gradient-to-tr from-purple-50 via-purple-100 to-purple-50">
